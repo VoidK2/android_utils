f9b51d387bc1ff13be38be12e6e1c2980a6a7fbd
zhangzexin <zhangzexin@top-stack.com>
2021-07-22 23:34:40 +0800

    dp和px转换

---
+++ DimensionUtil.java
@@ -0,0 +1,21 @@
+import android.content.Context;
+
+public class DimensionUtil {
+
+    public static DisplayMetrics getScreenDimensions(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics;
+    }
+
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static float px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
