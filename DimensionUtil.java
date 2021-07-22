f66e3fd3443e939ea7c130bb205bff6838b7ddd2
zhangzexin <zhangzexin@top-stack.com>
2021-07-22 23:37:47 +0800

    删除无用部分

--- DimensionUtil.java
+++ DimensionUtil.java
@@ -2,13 +2,6 @@ import android.content.Context;
 
 public class DimensionUtil {
 
-    public static DisplayMetrics getScreenDimensions(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        return displayMetrics;
-    }
-
     public static int dp2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
